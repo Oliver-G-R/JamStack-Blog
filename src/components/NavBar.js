6968d1906cbc0ad3d825322cd689fece8b636dcb
Oliver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2021-12-30 00:34:52 -0600

    ✍️ add: folders in styles to structuring and add vars for style blog

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+export const NavBar = () => {
+  return (
+        <>
+            <header>
+                <nav>
+                    <span>
+                        ogr
+                    </span>
+                    <ul>
+                        <li>
+                            <a href="">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Blog
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <style jsx>{`
+
+            `}</style>
+        </>
+  )
+}
